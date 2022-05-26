e2da7c699df5f265e2fabd63802aca6efaec8d9e
Richard Fisher <richardfsr@gmail.com>
2022-05-26 13:56:32 +0800

    try checking this is since nothing else is working

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/opt/app/production/provable"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "ecosystem.config.js"
+# append :linked_files, "ecosystem.config.js"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
