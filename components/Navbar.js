2a2ae8314d1c4d754093c3a169dcbea19c639dc8
Mike Wilcox <mwilcox56@gmail.com>
2022-08-28 20:06:58 -0400

    use large media query for nav header

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,8 +14,8 @@ export default function Navbar() {
 
   return (
     <header id="top" className="w-full flex flex-col">
-      <nav className="mb-4 flex flex-col md:flex-row w-full justify-between items-center">
-        <div className="w-full md:w-auto self-start md:self-center flex flex-row md:flex-none flex-no-wrap justify-between items-center">
+      <nav className="mb-4 flex flex-col lg:flex-row w-full justify-between items-center">
+        <div className="w-full lg:w-auto self-start lg:self-center flex flex-row lg:flex-none flex-no-wrap justify-between items-center">
           <h1 className="text-blue-500 hover:text-blue-600 text-xl font-bold inline">
             <Link href="/">
               <a>solprint</a>
@@ -23,7 +23,7 @@ export default function Navbar() {
           </h1>
           <button
             id="menuBtn"
-            className="hamburger block md:hidden focus:outline-none"
+            className="hamburger block lg:hidden focus:outline-none"
             type="button"
             onClick={navToggle}
           >
@@ -33,49 +33,49 @@ export default function Navbar() {
         </div>
         <div
           id="menu"
-          className="w-full md:w-auto self-end sm:self-center md:flex flex-col md:flex-row items-center h-fit py-1 pb-4 md:py-0 md:pb-0 hidden top-[40px] bg-white dark:bg-black"
+          className="w-full lg:w-auto self-end sm:self-center lg:flex flex-col lg:flex-row items-center h-fit py-1 pb-4 lg:py-0 lg:pb-0 hidden top-[40px] bg-white dark:bg-black"
         >
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/creator">
               <a className="text-blue-500 hover:text-blue-600">[Creator]</a>
             </Link>
           </div>
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/owner">
               <a className="text-blue-500 hover:text-blue-600">[Owner]</a>
             </Link>
           </div>
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/mint">
               <a className="text-blue-500 hover:text-blue-600">[Mint]</a>
             </Link>
           </div>
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/marketplaces">
               <a className="text-blue-500 hover:text-blue-600">
                 [Marketplaces]
               </a>
             </Link>
           </div>
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/collections">
               <a className="text-blue-500 hover:text-blue-600">[Collections]</a>
             </Link>
           </div>
-          {/* <div className="mr-6 mb-6 md:mb-0 inline">
+          {/* <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/listings">
               <a className="text-blue-500 hover:text-blue-600">[Listings]</a>
             </Link>
           </div> */}
-          <div className="mr-6 mb-6 md:mb-0 inline">
+          <div className="mr-6 mb-6 lg:mb-0 inline">
             <Link href="/wallet">
               <a className="text-blue-500 hover:text-blue-600">[Wallet]</a>
             </Link>
           </div>
-          <DarkMode classname={["mb-6 md:mb-0 inline"]} />
+          <DarkMode classname={["mb-6 lg:mb-0 inline"]} />
 
-          <div className="mr-6 mb-6 md:mb-0 inline wallet-btn">
-            <WalletMultiButton style={{ height: 30, fontSize: 13 }} />
+          <div className="mr-6 mb-6 lg:mb-0 inline wallet-btn">
+            <WalletMultiButton />
           </div>
         </div>
       </nav>
